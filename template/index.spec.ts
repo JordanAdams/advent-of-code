91373b92f6c1050c626ab8bc4154ea66b9064849
Jordan Adams <hi@jordanadams.co.uk>
2022-12-05 17:01:47 +0000

    Adds template to generate days

---
+++ template/index.spec.ts
@@ -0,0 +1,21 @@
+import { part1, part2 } from ".";
+import { exampleInput, generatedInput } from "./input";
+
+describe("part1", () => {
+  it("should work for example input", () => {
+    expect(part1(exampleInput)).toBe(undefined);
+  });
+
+  // it("should work for generated input", () => {
+  //   expect(part1(generatedInput)).toBe(undefined);
+  // });
+});
+
+describe("part2", () => {
+  // it("should work for example input", () => {
+  //   expect(part2(exampleInput)).toBe(undefined);
+  // });
+  // it("should work for generated input", () => {
+  //   expect(part2(generatedInput)).toBe(undefined);
+  // });
+});
