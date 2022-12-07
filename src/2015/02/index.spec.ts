ec7376d74b10a7e7cc64171a2ebdaecbd9af7775
Jordan Adams <hi@jordanadams.co.uk>
2022-12-07 10:15:37 +0000

    Completed 2015 02

--- src/2015/02/index.spec.ts
+++ src/2015/02/index.spec.ts
@@ -3,19 +3,20 @@ import { exampleInput, generatedInput } from "./input";
 
 describe("part1", () => {
   it("should work for example input", () => {
-    expect(part1(exampleInput)).toBe(undefined);
+    expect(part1(exampleInput)).toBe(101);
   });
 
-  // it("should work for generated input", () => {
-  //   expect(part1(generatedInput)).toBe(undefined);
-  // });
+  it("should work for generated input", () => {
+    expect(part1(generatedInput)).toBe(1598415);
+  });
 });
 
 describe("part2", () => {
-  // it("should work for example input", () => {
-  //   expect(part2(exampleInput)).toBe(undefined);
-  // });
-  // it("should work for generated input", () => {
-  //   expect(part2(generatedInput)).toBe(undefined);
-  // });
+  it("should work for example input", () => {
+    expect(part2(exampleInput)).toBe(48);
+  });
+
+  it("should work for generated input", () => {
+    expect(part2(generatedInput)).toBe(3812909);
+  });
 });
--- src/2015/02/index.ts
+++ src/2015/02/index.ts
@@ -1,7 +1,34 @@
-export const part1 = () => {
-  // TODO
+type Box = [number, number, number];
+
+const sum = (xs: number[]): number => xs.reduce((acc, n) => acc + n, 0);
+
+const calculateBox = (l: number, w: number, h: number): number => {
+  const areas = [l * w, w * h, h * l];
+
+  return sum(areas) * 2 + Math.min(...areas);
 };
 
-export const part2 = () => {
-  // TODO
+const calculateRibbon = (l: number, w: number, h: number): number => {
+  const [s1, s2] = [l, w, h].sort((a, b) => a - b);
+
+  return 2 * s1 + 2 * s2 + l * w * h;
+};
+
+const parseInput = (input: string): Box[] => {
+  return input.split("\n").map((line): Box => {
+    const [l, w, h] = line.split("x");
+    return [parseInt(l, 10), parseInt(w, 10), parseInt(h, 10)];
+  });
+};
+
+export const part1 = (input: string): number => {
+  const boxes = parseInput(input);
+  const paper = boxes.map((box) => calculateBox(...box));
+  return sum(paper);
+};
+
+export const part2 = (input: string): number => {
+  const boxes = parseInput(input);
+  const ribbon = boxes.map((box) => calculateRibbon(...box));
+  return sum(ribbon);
 };
